e4cae208ae08e394ad35d5ff3858d23220072252
Johannes Habel <EchterAlsFake@proton.me>
2025-01-30 05:51:38 +0100

    - function for Porn Fetch

--- xvideos_api/xvideos_api.py
+++ xvideos_api/xvideos_api.py
@@ -44,6 +44,11 @@ logging.basicConfig(format='%(name)s %(levelname)s %(asctime)s %(message)s', dat
 logger = logging.getLogger("XVIDEOS API")
 logger.setLevel(logging.DEBUG)
 
+def refresh_core(): # Needed for Porn Fetch
+    global core
+    core = BaseCore()
+
+
 def disable_logging():
     logger.setLevel(logging.CRITICAL)
 
